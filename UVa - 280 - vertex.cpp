762a89d3c8b3fb6a02aa709277def3e22c173fe4
rkshaon <rkshaon.ist@gmail.com>
2017-08-25 00:14:56 +0600

    uva 280 modified but not solved

--- UVa - 280 - vertex.cpp
+++ UVa - 280 - vertex.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(){
     int n;
     while(scanf("%d", &n), n!=0){
-        int graph[n+1][n+1] = {0};
+        int graph[n+1][n+1] = {0}; // initialized zero
         int startNode;
         int endNode;
         /*
@@ -18,10 +18,10 @@ int main(){
         }
         */
         while(scanf("%d", &startNode), startNode!=0){
-            graph[startNode][0]=n;
+            //graph[startNode][0]=n; // first value contains the total node number
             while(scanf("%d", &endNode), endNode!=0){
-                graph[startNode][endNode]=1;
-                graph[startNode][0]--;
+                graph[startNode][endNode]=1; // replaced by one for connection
+                //graph[startNode][0]--;
             }
         }
         /*
@@ -37,9 +37,14 @@ int main(){
         int q;
         while(queryNo--){
             scanf("%d", &q);
-            printf("%d", graph[q][0]);
+            int c=0;
             for(int i=1; i<=n; i++){
-                if(graph[q][i]==0) printf(" %d", i);
+                if(graph[q][i]==0) c++; // count total no edge node
+            }
+            //printf("%d", graph[q][0]);
+            printf("%d", c);
+            for(int i=1; i<=n; i++){
+                if(graph[q][i]==0) printf(" %d", i); // printing no edge node
             }
             printf("\n");
         }
