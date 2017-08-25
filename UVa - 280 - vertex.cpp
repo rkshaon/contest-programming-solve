c9fbac04c002444d49e9eaf62a9b3904c56e191d
rkshaon <rkshaon.ist@gmail.com>
2017-08-25 23:33:31 +0600

    uva 280 attempt again

--- UVa - 280 - vertex.cpp
+++ UVa - 280 - vertex.cpp
@@ -3,48 +3,53 @@
 
 using namespace std;
 
+// global declaration
+int n; // number of node
+vector<int>graph[101]; // declaring graph
+bool visited[101] = {false}; // checking is visited or not
+
+// checking graph input
+void checkingInput(){
+    for(int i=1; i<=n; i++){
+        for(int j=0; j<graph[i].size(); j++){
+            printf("%d ", graph[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void dfs(int startingNode){
+    visited[startingNode]=true; // marking as visited
+    for(int i=0; i<graph[startingNode].size(); i++){
+        if(!visited[graph[startingNode][i]]){
+            dfs(graph[startingNode][i]); // again calling dfs with adjacent value
+        }
+    }
+}
+
 int main(){
-    int n;
     while(scanf("%d", &n), n!=0){
-        int graph[n+1][n+1] = {0}; // initialized zero
-        int startNode;
-        int endNode;
-        /*
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                printf("%d ", graph[i][j]);
-            }
-            printf("\n");
-        }
-        */
-        while(scanf("%d", &startNode), startNode!=0){
-            //graph[startNode][0]=n; // first value contains the total node number
-            while(scanf("%d", &endNode), endNode!=0){
-                graph[startNode][endNode]=1; // replaced by one for connection
-                //graph[startNode][0]--;
+        int sNode; // start node of edge
+        while(scanf("%d", &sNode), sNode!=0){
+            int eNode; // end node of edge
+            while(scanf("%d", &eNode), eNode!=0){
+                graph[sNode].push_back(eNode);
             }
         }
-        /*
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                printf("%d ", graph[i][j]);
-            }
-            printf("\n");
-        }
-        */
+        //checkingInput();
         int queryNo;
         scanf("%d", &queryNo);
-        int q;
-        while(queryNo--){
+        for(int i=1; i<=queryNo; i++){
+            int q;
             scanf("%d", &q);
+            dfs(q);
             int c=0;
             for(int i=1; i<=n; i++){
-                if(graph[q][i]==0) c++; // count total no edge node
+                if(!visited[i]) c++;
             }
-            //printf("%d", graph[q][0]);
             printf("%d", c);
             for(int i=1; i<=n; i++){
-                if(graph[q][i]==0) printf(" %d", i); // printing no edge node
+                if(!visited[i]) printf(" %d", i);
             }
             printf("\n");
         }
